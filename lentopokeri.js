b8d5969e473f8b4126c159ef7b799fd0687b2d02
Tatu <tikka.s.tatu@gmail.com>
2023-01-08 20:08:07 +0200

    Päivitystä. lisätty rng

--- lentopokeri.js
+++ lentopokeri.js
@@ -2,7 +2,6 @@
 /* 1. show map using Leaflet library. (L comes from the Leaflet library) */
 
 
-
 const API_KEY = '91c48c77162abbfc79f78f194a056dff';
 let city = 'Helsinki';
 let city2 = 'Prague';
@@ -12,16 +11,16 @@ let city5 = 'Seoul';
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
 
-    const weatherConditions = data.weather[0].description;
-    const popupContent = `
+        const weatherConditions = data.weather[0].description;
+        const popupContent = `
       <h2>${data.name}</h2>
       <p>${weatherConditions}</p>
     `;
-     EFHK.bindPopup(popupContent);
-  });
+        EFHK.bindPopup(popupContent);
+    });
 
 const map = L.map('map').setView([60.316998732, 24.957996168], 5)
 L.tileLayer('https://api.maptiler.com/maps/voyager-v2/{z}/{x}/{y}.png?key=CM8RQD53fPqBa46Rjo28', {
@@ -29,13 +28,11 @@ L.tileLayer('https://api.maptiler.com/maps/voyager-v2/{z}/{x}/{y}.png?key=CM8RQD
 }).addTo(map);
 
 
-
 let EFHK = L.marker([60.316998732, 24.957996168]).addTo(map);
 let LKPR = L.marker([50.100499598, 14.255998976]).addTo(map);
 let HECA = L.marker([30.119832854, 31.40333172]).addTo(map);
 let VOBL = L.marker([13.192665896, 77.70333052]).addTo(map);
 let RKSI = L.marker([37.4582357, 126.4763689]).addTo(map);
-
 
 
 EFHK.bindPopup("<b>Helsinki</b><br>");
@@ -46,16 +43,56 @@ RKSI.bindPopup("<b>Seoul</b><br>I am a popup.");
 
 const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${city2}&appid=${API_KEY}`;
 fetch(url2)
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
 
-    const weatherConditions = data.weather[0].description;
-    const popupContent = `
+        const weatherConditions = data.weather[0].description;
+        const popupContent = `
       <h2>${data.name}</h2>
       <p>${weatherConditions}</p>
     `;
-     LKPR.bindPopup(popupContent);
-  });
+        LKPR.bindPopup(popupContent);
+    });
+
+const url3 = `https://api.openweathermap.org/data/2.5/weather?q=${city3}&appid=${API_KEY}`;
+fetch(url3)
+    .then(response => response.json())
+    .then(data => {
+
+        const weatherConditions = data.weather[0].description;
+        const popupContent = `
+      <h2>${data.name}</h2>
+      <p>${weatherConditions}</p>
+    `;
+        HECA.bindPopup(popupContent);
+    });
+
+const url4 = `https://api.openweathermap.org/data/2.5/weather?q=${city4}&appid=${API_KEY}`;
+fetch(url4)
+    .then(response => response.json())
+    .then(data => {
+
+        const weatherConditions = data.weather[0].description;
+        const popupContent = `
+      <h2>${data.name}</h2>
+      <p>${weatherConditions}</p>
+    `;
+        VOBL.bindPopup(popupContent);
+    });
+
+const url5 = `https://api.openweathermap.org/data/2.5/weather?q=${city5}&appid=${API_KEY}`;
+fetch(url5)
+    .then(response => response.json())
+    .then(data => {
+
+        const weatherConditions = data.weather[0].description;
+        const popupContent = `
+      <h2>${data.name}</h2>
+      <p>${weatherConditions}</p>
+    `;
+        RKSI.bindPopup(popupContent);
+    });
+
 
 // global variables
 const apiUrl = 'http://127.0.0.1:5000/';
